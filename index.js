673ff70392fc60d47366f6355bf3b34f9e4e636d
Tobias Thiele <thiele231@web.de>
2023-06-27 04:40:53 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,66 @@
+module.exports = {
+  extends: [ "@antfu" ],
+  plugins: [],
+  rules: {
+    // TypeScript
+    "@typescript-eslint/quotes": [
+      "error",
+      "double",
+    ],
+    "@typescript-eslint/semi": [
+      "error",
+      "always",
+    ],
+    "@typescript-eslint/comma-dangle": [
+      "error",
+      "always-multiline",
+    ],
+
+    // JavaScript
+    "quotes": [
+      "error",
+      "double",
+    ],
+    "semi": [
+      "error",
+      "always",
+    ],
+    "comma-dangle": [
+      "error",
+      "always-multiline",
+    ],
+    "object-curly-spacing": [
+      "error",
+      "always",
+    ],
+    "array-bracket-spacing": [
+      "error",
+      "always",
+    ],
+
+    // Vue
+    "vue/first-attribute-linebreak": [ "error", {
+      singleline: "ignore",
+      multiline: "below",
+    } ],
+    "vue/component-tags-order": [ "error", {
+      order: [ "docs", "template", "script:not([setup])", "script[setup]", "style[scoped]", "style:not([scoped])" ],
+    } ],
+    "vue/attributes-order": [ "error", {
+      order: [
+        "EVENTS",
+        "DEFINITION",
+        "LIST_RENDERING",
+        "CONDITIONALS",
+        "RENDER_MODIFIERS",
+        "GLOBAL",
+        [ "UNIQUE", "SLOT" ],
+        "TWO_WAY_BINDING",
+        "OTHER_DIRECTIVES",
+        "OTHER_ATTR",
+        "CONTENT",
+      ],
+      alphabetical: false,
+    } ],
+  },
+};
