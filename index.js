eb2de8ab9281f103bca3896d6fc8e3163ceaee9a
Tobias Thiele <thiele231@web.de>
2023-07-11 12:12:13 +0200

    add curly rule

--- index.js
+++ index.js
@@ -1,36 +1,39 @@
-const { isPackageExists } = require('local-pkg');
-const TS = isPackageExists('typescript');
+const { isPackageExists } = require("local-pkg");
 
-const TS_RULES = TS ? {
-  "@typescript-eslint/quotes": [
-    "error",
-    "double",
-  ],
-  "@typescript-eslint/semi": [
-    "error",
-    "always",
-  ],
-  "@typescript-eslint/comma-dangle": [
-    "error",
-    "always-multiline",
-  ],
-  "@typescript-eslint/member-delimiter-style": ["error", {
-    "multiline": {
-      "delimiter": "semi",
-      "requireLast": true
-    },
-    "singleline": {
-      "delimiter": "semi",
-      "requireLast": false
-    },
-    "multilineDetection": "brackets",
-  }],
-  "@typescript-eslint/no-console": "off",
-  "@typescript-eslint/brace-style": ["error", "1tbs", { "allowSingleLine": true }],
-  "@typescript-eslint/ban-ts-comment": "off",
-} : {
-  "brace-style": ["error", "1tbs", { "allowSingleLine": true }],
-};
+const TS = isPackageExists("typescript");
+
+const TS_RULES = TS
+  ? {
+    "@typescript-eslint/quotes": [
+      "error",
+      "double",
+    ],
+    "@typescript-eslint/semi": [
+      "error",
+      "always",
+    ],
+    "@typescript-eslint/comma-dangle": [
+      "error",
+      "always-multiline",
+    ],
+    "@typescript-eslint/member-delimiter-style": [ "error", {
+      multiline: {
+        delimiter: "semi",
+        requireLast: true,
+      },
+      singleline: {
+        delimiter: "semi",
+        requireLast: false,
+      },
+      multilineDetection: "brackets",
+    } ],
+    "@typescript-eslint/no-console": "off",
+    "@typescript-eslint/brace-style": [ "error", "1tbs", { allowSingleLine: true } ],
+    "@typescript-eslint/ban-ts-comment": "off",
+  }
+  : {
+    "brace-style": [ "error", "1tbs", { allowSingleLine: true } ],
+  };
 
 module.exports = {
   extends: [ "@antfu" ],
@@ -61,17 +64,18 @@ module.exports = {
       "always",
     ],
     "no-console": "off",
+    "curly": [ "error", "multi-line", "consistent" ],
 
     // Vue
     "vue/first-attribute-linebreak": [ "error", {
-      "singleline": "ignore",
-      "multiline": "below",
+      singleline: "ignore",
+      multiline: "below",
     } ],
     "vue/component-tags-order": [ "error", {
-      "order": [ "docs", "template", "script:not([setup])", "script[setup]", "style[scoped]", "style:not([scoped])" ],
+      order: [ "docs", "template", "script:not([setup])", "script[setup]", "style[scoped]", "style:not([scoped])" ],
     } ],
     "vue/attributes-order": [ "error", {
-      "order": [
+      order: [
         "EVENTS",
         "DEFINITION",
         "LIST_RENDERING",
@@ -84,7 +88,7 @@ module.exports = {
         "OTHER_ATTR",
         "CONTENT",
       ],
-      "alphabetical": false,
+      alphabetical: false,
     } ],
   },
 };
